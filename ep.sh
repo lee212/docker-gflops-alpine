c1024f53c10248f3bb2d12c988793e45ea922c73
Hyungro Lee <hroe.lee@gmail.com>
2018-02-20 23:34:24 -0500

    date "+%Y-%m-%d %H:%M:%S.%s"

--- ep.sh
+++ ep.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 msg=`python /flops.py $@`
 uptime=`uptime`
-curr_time=`date`
+curr_time=`date "+%Y-%m-%d %H:%M:%S.%s"`
 echo "{\"system\": {\"hostname\":\"$HOSTNAME\", \"uptime\":\"$uptime\", \"curr_time\":\"$curr_time\"}, \"msg\": \"$msg\", \"arg\":$@}"
